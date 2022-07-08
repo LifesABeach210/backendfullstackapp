fb1481ab6d2e1d8ed3379c939b2c7f412a3fc144
Daniel Rothwell <danielrothwell@Daniels-MacBook-Pro.local>
2022-07-08 11:24:21 -0400

    07/08/2022

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,25 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const { blogsDB } = require("../mongo");
 
+router.get("/hello-blogs", function (req, res, next) {
+  res.send({ message: "hello from express" });
+});
 
+router.get("/all-blogs", async function (req, res, next) {
+  const limit = Number(req.query.limit);
+  const skip = Number(req.query.limit)(Number(req.query.page) - 1);
+  const sortField = req.query.sortField;
+  const sortOrder = req.query.sortOrder;
+  const filterField = req.query.filterField;
+  const filterValue = req.query.filterValue;
 
-router.get('/hello-blogs',function(req,res,next){
-    res.send({message:"hello from express"});
-})
-
-router.get("/all-blogs",async function(req,res,next){
-   try{ const collection = await blogsDB().collection("post");
+  try {
+    const collection = await blogsDB().collection("post");
     const allBlogs = await collection.find().toArray();
     res.json(allBlogs);
-
-}catch(e){
+  } catch (e) {
     console.error(e);
-}
-
-
-})
-
-
+  }
+});
 
 module.exports = router;
